c8522968c8dc2e72b268a5153ffb8418e7eba31f
James Knight <knight.of.design@gmail.com>
2018-03-31 09:40:59 -0400

    Add KanbanColumn test

---
+++ src/components/KanbanColumn/KanbanColumn.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import KanbanColumn from './KanbanColumn';
+import data from '../../data'
+
+test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<KanbanColumn taskList={data.taskList} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+test('renders a Kanban column', () => {
+
+    const component = renderer.create(<KanbanColumn taskList={data.taskList}/>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
--- src/models/TaskList.js
+++ src/models/TaskList.js
@@ -1,6 +1,13 @@
 import {observable} from 'mobx'
 
 class TaskList {
+    static of(...tasks){
+       const result = new TaskList();
+        tasks.forEach((task)=>{
+            result.addTask(task)
+        })
+    }
+
     @observable tasks = [];
 
     addTask(task) {
